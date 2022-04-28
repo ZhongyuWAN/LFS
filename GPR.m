0cc63e84c0ea3c3c5c61de9d9dc8e99a488afff0
ZhongyuWAN <91055401+ZhongyuWAN@users.noreply.github.com>
2022-04-28 21:50:34 +0800

    Add files via upload

---
+++ GPR.m
@@ -0,0 +1,23 @@
+function [trainedModel, validationRMSE] = trainRegressionModel(trainingData)
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6'});
+predictorNames = {'column_1', 'column_2', 'column_3', 'column_4', 'column_5'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_6;
+isCategoricalPredictor = [false, false, false, false, false];
+regressionGP = fitrgp(...
+    predictors, ...
+    response, ...
+    'BasisFunction', 'constant', ...
+    'KernelFunction', 'matern52', ...
+    'Standardize', true);
+predictorExtractionFcn = @(x) array2table(x, 'VariableNames', predictorNames);
+gpPredictFcn = @(x) predict(regressionGP, x);
+trainedModel.predictFcn = @(x) gpPredictFcn(predictorExtractionFcn(x));
+trainedModel.RegressionGP = regressionGP;
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6'});
+predictorNames = {'column_1', 'column_2', 'column_3', 'column_4', 'column_5'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_6;
+isCategoricalPredictor = [false, false, false, false, false];
+validationPredictions = predict(trainedModel.RegressionGP, predictors);
+validationRMSE = sqrt(resubLoss(trainedModel.RegressionGP, 'LossFun', 'mse'));
